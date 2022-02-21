4de6dd1b15244ce5c1a9142884411bffeb3317e0
lovelycbm <lovelycbm@naver.com>
2022-02-21 23:24:56 +0900

    참조자, 빌림, 가변참조, 댕글링 참조

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,15 @@
 
 fn main() {   
-    let x = plus_one(5);
-
-    println!("The value of x is {}",x);
+    let mut s = String::from("hello");
+    
+    let r1 = &s;
+    let r2 = &s;
+    let r3 = &mut s;
+    // change(&mut s);
+    println!("{} {}",r1,r2)
 }
 
-fn plus_one(x: i32) -> i32 {
-    // 반환 부분이 세미콜론이 없는것으로 이해하면 됨.
-    let y = x+1;
-    y
+fn change(some_string : &mut String) {
+    some_string.push_str(", world");
 }
+
