5f91970a03884f35676afc59e9174c7bfc173e8a
lovelycbm <lovelycbm@naver.com>
2022-03-20 18:15:26 +0900

    구조체 및 메서드

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,39 +1,26 @@
-struct  User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32
 }
 
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width        
+    }
+    fn can_hold(&self, other: &Rectangle)-> bool {
+        self.length > other.length && self.width > other.width
+    }
+    fn square(size:u32) -> Rectangle {
+        Rectangle { length : size, width: size}
+    }
+}
 
 fn main() {       
-    let mut user1 = User {
-        email : String::from("lovelycbm@naver.com"),
-        username : String::from("lovelycbm"),
-        sign_in_count : 1,
-        active : true
-    };
-    
-    user1.email = String::from("lovelybm@hanmail.net");
-    
-    let user2 = User {
-        email : String::from("test@test.com"),
-        username : String::from("test"),
-        ..user1
-    };
-
-    let black = Color(0, 0, 0);
-    let origin = PointI(0,0,0);
-}
-
-
-fn build_user(email:String, username:String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+    let rect1 = Rectangle { length: 50, width:30};
+    let rect2 = Rectangle { length: 40, width:10};
+    let rect3 = Rectangle { length: 45, width:60};
+           
+    println!("Can rect1 hold rect2? {}",rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}",rect1.can_hold(&rect3));
 }
